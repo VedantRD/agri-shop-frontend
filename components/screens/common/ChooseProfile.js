1d3d4115052ae5d21299e9ee869c4854f5c5503c
BhutadaRohan <gopalbhutada2221@gmail.com>
2021-05-16 09:20:00 +0530

    UI changes on choose profle page

--- components/screens/common/ChooseProfile.js
+++ components/screens/common/ChooseProfile.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import { Image, StyleSheet, View, StatusBar } from 'react-native'
 import { Card, Layout, Text, Icon, StyleService, useStyleSheet, Button } from '@ui-kitten/components';
 import { ChevronRightIcon } from './Icons';
+import snackbar from '../common/Snackbar'
 
 const ChooseProfile = ({ navigation }) => {
 
     const styles = useStyleSheet(themedStyles)
     const [selectedIndex, setSelectedIndex] = useState(null)
+
+    const moveNext = () => {
+        if (selectedIndex !== null) {
+            selectedIndex === 0 ? navigation.navigate("BUYER_LOGIN") : navigation.navigate("SELLER_LOGIN")
+        } else {
+            snackbar({ type: 'failed', message: "You have to choose one profile" })
+        }
+    }
 
     return (
         <Layout level='4' style={styles.container}>
@@ -24,6 +33,10 @@ const ChooseProfile = ({ navigation }) => {
             }}>
                 AgriShop
             </Text> */}
+            <StatusBar
+                backgroundColor="rgba(98, 0, 238, 0.08)"
+                barStyle="light-content"
+            />
             <View style={styles.logoContainer}>
                 <Image source={require('../../assets/shop-1.jpg')} style={styles.logo} />
             </View>
@@ -37,10 +50,13 @@ const ChooseProfile = ({ navigation }) => {
                         <Image source={require('../../assets/customer-1.jpg')} style={styles.cardImage} />
                         <View style={styles.cardText}>
                             <Text category='h5' style={styles.cardText}>Customer</Text>
+                            <Text appearance='hint' category="c1" style={[styles.cardText, { marginRight: 90 }]}>
+                                Want to buy fresh vegetables and friuts at reasonable rates from direct farmer delivered at home safely
+                            </Text>
                         </View>
                     </View>
                 </Card>
-                <Text category='h6' appearance='hint' style={{ textAlign: 'center', marginBottom: 15 }}>OR</Text>
+                <Text category='h6' appearance='hint' style={{ textAlign: 'center', marginBottom: 15 }}>----------  OR  ---------</Text>
                 <Card
                     style={[styles.card, selectedIndex == 1 && styles.activeCard]}
                     onPress={() => setSelectedIndex(1)}
@@ -49,12 +65,19 @@ const ChooseProfile = ({ navigation }) => {
                         <Image source={require('../../assets/farmer-1.jpg')} style={styles.cardImage} />
                         <View style={styles.cardText}>
                             <Text category='h5' style={styles.cardText}>Farmer</Text>
+                            <Text appearance='hint' category="c1" style={[styles.cardText, { marginRight: 90 }]}>
+                                Want to sell farm products such as vegetables,fruits and more things directly to the customers
+                            </Text>
                         </View>
                     </View>
                 </Card>
             </View>
             <View style={styles.buttonContainer}>
-                <Button accessoryRight={ChevronRightIcon} size='giant' style={styles.button}>
+                <Button
+                    accessoryRight={ChevronRightIcon}
+                    size='medium'
+                    style={styles.button}
+                    onPress={moveNext}>
                     {/* <Text category='h6' status='control'>NEXT</Text> */}
                 Next
             </Button>
@@ -69,7 +92,8 @@ const themedStyles = StyleService.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        padding: 15
+        padding: 15,
+        backgroundColor: 'rgba(98, 0, 238, 0.08)'
     },
     logoContainer: {
         flex: 2,
@@ -96,7 +120,9 @@ const themedStyles = StyleService.create({
     button: {
         width: '40%',
         alignSelf: 'flex-end',
+        marginBottom: '3%',
         // backgroundColor: '#fff',
+        borderRadius: 24,
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -115,7 +141,7 @@ const themedStyles = StyleService.create({
         textAlignVertical: 'top',
         marginBottom: 15,
         paddingLeft: -16,
-        shadowColor: "#000",
+        shadowColor: "color-primary-default",
         shadowOffset: {
             width: 0,
             height: 1,
@@ -123,11 +149,21 @@ const themedStyles = StyleService.create({
         shadowOpacity: 0.22,
         shadowRadius: 2.22,
         elevation: 1,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 30,
+        borderBottomRightRadius: 8,
+        borderBottomLeftRadius: 30
     },
     activeCard: {
-        borderWidth: 2,
-        borderColor: 'color-primary-default',
+        // borderWidth: 2,
+        borderWidth: 0,
+        borderRightWidth: 8,
+        borderColor: 'color-primary-400',
         elevation: 5,
+        shadowOffset: {
+            width: 3,
+            height: 6,
+        },
     },
     cardImage: {
         height: 125,
